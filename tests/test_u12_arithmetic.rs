db919b790c4323c579b1aa11549d2f38ed775055
Martin Mroz <martinmroz@gmail.com>
2016-06-17 12:59:12 -0400

    Rewrite Arithmetic tests in terms of u12![] macro.

--- tests/test_u12_arithmetic.rs
+++ tests/test_u12_arithmetic.rs
@@ -73,178 +73,157 @@ fn test_overflowing_add() {
 
 #[test]
 fn test_sub_operator() {
-  let zero = U12::min_value();
-  let  one = U12::from(1u8);
-  let  max = U12::max_value();
-
-  assert_eq!(zero - zero, zero);
-  assert_eq!(one - zero, one);
-  assert_eq!(max - zero, max);
-  assert_eq!(max - one, (0xFFE as u16).unchecked_into());
+  assert_eq!(u12![0] - u12![0], u12![0]);
+  assert_eq!(u12![1] - u12![0], u12![1]);
+  assert_eq!(U12::max_value() - u12![0], U12::max_value());
+  assert_eq!(U12::max_value() - u12![1], u12![0xFFE]);
 }
 
 #[test]
 #[should_panic]
 fn test_sub_operator_underflow() {
-  let one = U12::from(1u8);
-  let min = U12::min_value();
-  let _ = min - one;
+  let _ = U12::min_value() - u12![1];
 }
 
 #[test]
 fn test_checked_sub() {
-  let zero = U12::min_value();
-  let  one = U12::from(1u8);
-  let  two = U12::from(2u8);
-  let  max = U12::max_value();
-
-  assert_eq!(zero.checked_sub(zero), Some(zero));
-  assert_eq!(zero.checked_sub(one), None);
-  assert_eq!(one.checked_sub(zero), Some(one));
-  assert_eq!(zero.checked_sub(max), None);
-  assert_eq!(max.checked_sub(zero), Some(max));
-  assert_eq!(max.checked_sub(one), Some((0xFFE as u16).unchecked_into()));
-  assert_eq!(max.checked_sub(two), Some((0xFFD as u16).unchecked_into()));
-  assert_eq!(max.checked_sub(max), Some(zero));
+  assert_eq!(u12![0].checked_sub(u12![0]), Some(u12![0]));
+  assert_eq!(u12![0].checked_sub(u12![1]), None);
+  assert_eq!(u12![1].checked_sub(u12![0]), Some(u12![1]));
+  assert_eq!(u12![0].checked_sub(U12::max_value()), None);
+  assert_eq!(U12::max_value().checked_sub(u12![0]), Some(U12::max_value()));
+  assert_eq!(U12::max_value().checked_sub(u12![1]), Some(u12![0xFFE]));
+  assert_eq!(U12::max_value().checked_sub(u12![2]), Some(u12![0xFFD]));
+  assert_eq!(U12::max_value().checked_sub(U12::max_value()), Some(u12![0]));
 }
 
 #[test]
 fn test_saturating_sub() {
-  let zero = U12::min_value();
-  let  one = U12::from(1u8);
-  let  two = U12::from(2u8);
-  let  max = U12::max_value();
-
-  assert_eq!(zero.saturating_sub(zero), zero);
-  assert_eq!(zero.saturating_sub(one), zero);
-  assert_eq!(one.saturating_sub(zero), one);
-  assert_eq!(zero.saturating_sub(max), zero);
-  assert_eq!(max.saturating_sub(zero), max);
-  assert_eq!(max.saturating_sub(one), (0xFFE as u16).unchecked_into());
-  assert_eq!(max.saturating_sub(two), (0xFFD as u16).unchecked_into());
-  assert_eq!(max.saturating_sub(max), zero);
+  assert_eq!(u12![0].saturating_sub(u12![0]), u12![0]);
+  assert_eq!(u12![0].saturating_sub(u12![1]), u12![0]);
+  assert_eq!(u12![1].saturating_sub(u12![0]), u12![1]);
+  assert_eq!(u12![0].saturating_sub(U12::max_value()), u12![0]);
+  assert_eq!(U12::max_value().saturating_sub(u12![0]), U12::max_value());
+  assert_eq!(U12::max_value().saturating_sub(u12![1]), u12![0xFFE]);
+  assert_eq!(U12::max_value().saturating_sub(u12![2]), u12![0xFFD]);
+  assert_eq!(U12::max_value().saturating_sub(U12::max_value()), u12![0]);
 }
 
 #[test]
 fn test_wrapping_sub() {
-  let zero = U12::min_value();
-  let  one = U12::from(1u8);
-  let  two = U12::from(2u8);
-  let  max = U12::max_value();
-
-  assert_eq!(zero.wrapping_sub(zero), zero);
-  assert_eq!(zero.wrapping_sub(one), max);
-  assert_eq!(one.saturating_sub(zero), one);
-  assert_eq!(zero.wrapping_sub(max), one);
-  assert_eq!(max.wrapping_sub(zero), max);
-  assert_eq!(max.wrapping_sub(one), (0xFFE as u16).unchecked_into());
-  assert_eq!(max.wrapping_sub(two), (0xFFD as u16).unchecked_into());
-  assert_eq!(max.wrapping_sub(max), zero);
+  assert_eq!(u12![0].wrapping_sub(u12![0]), u12![0]);
+  assert_eq!(u12![0].wrapping_sub(u12![1]), U12::max_value());
+  assert_eq!(u12![1].saturating_sub(u12![0]), u12![1]);
+  assert_eq!(u12![0].wrapping_sub(U12::max_value()), u12![1]);
+  assert_eq!(U12::max_value().wrapping_sub(u12![0]), U12::max_value());
+  assert_eq!(U12::max_value().wrapping_sub(u12![1]), u12![0xFFE]);
+  assert_eq!(U12::max_value().wrapping_sub(u12![2]), u12![0xFFD]);
+  assert_eq!(U12::max_value().wrapping_sub(U12::max_value()), u12![0]);
 }
 
 // MARK: - Tests - Multiplication
 
 #[test]
 fn test_mul_operator() {
-  assert_eq!(U12::from(2u8) * U12::from(0u8), U12::min_value());
+  assert_eq!(u12![2] * u12![0], U12::min_value());
   assert_eq!(U12::max_value() * U12::min_value(), U12::min_value());
-  assert_eq!(U12::from(2u8) * U12::from(255u8), (510 as u16).unchecked_into());
-  assert_eq!(U12::from(255u8) * U12::from(2u8), (510 as u16).unchecked_into());
+  assert_eq!(u12![2] * u12![255], u12![510]);
+  assert_eq!(u12![255] * u12![2], u12![510]);
 }
 
 #[test]
 #[should_panic]
 fn test_mul_operator_overflow() {
-  let _ = U12::from(2u8) * 2048u16.unchecked_into();
+  let _ = u12![2] * u12![2048];
 }
 
 #[test]
 fn test_checked_mul() {
-  assert_eq!(U12::from(2u8).checked_mul(0u8.into()), Some(U12::min_value()));
-  assert_eq!(U12::from(2u8).checked_mul(255u8.into()), Some((510 as u16).unchecked_into()));
-  assert_eq!(U12::from(255u8).checked_mul(2u8.into()), Some((510 as u16).unchecked_into()));
-  assert_eq!(U12::from(2u8).checked_mul((2048u16).unchecked_into()), None);
-  assert_eq!(U12::from(2u8).checked_mul((4095u16).unchecked_into()), None);
+  assert_eq!(u12![2].checked_mul(u12![0]), Some(U12::min_value()));
+  assert_eq!(u12![2].checked_mul(u12![255]), Some(u12![510]));
+  assert_eq!(u12![255].checked_mul(u12![2]), Some(u12![510]));
+  assert_eq!(u12![2].checked_mul(u12![2048]), None);
+  assert_eq!(u12![2].checked_mul(u12![4095]), None);
 }
 
 #[test]
 fn test_saturating_mul() {
-  assert_eq!(U12::from(2u8).saturating_mul(0u8.into()), U12::min_value());
-  assert_eq!(U12::from(2u8).saturating_mul(255u8.into()), (510 as u16).unchecked_into());
-  assert_eq!(U12::from(255u8).saturating_mul(2u8.into()), (510 as u16).unchecked_into());
-  assert_eq!(U12::from(2u8).saturating_mul((2048u16).unchecked_into()), U12::max_value());
-  assert_eq!(U12::from(2u8).saturating_mul((4095u16).unchecked_into()), U12::max_value());
+  assert_eq!(u12![2].saturating_mul(u12![0]), U12::min_value());
+  assert_eq!(u12![2].saturating_mul(u12![255]), u12![510]);
+  assert_eq!(u12![255].saturating_mul(u12![2]), u12![510]);
+  assert_eq!(u12![2].saturating_mul(u12![2048]), U12::max_value());
+  assert_eq!(u12![2].saturating_mul(u12![4095]), U12::max_value());
 }
 
 #[test]
 fn test_wrapping_mul() {
-  assert_eq!(U12::from(2u8).wrapping_mul(0u8.into()), U12::min_value());
-  assert_eq!(U12::from(2u8).wrapping_mul(255u8.into()), (510 as u16).unchecked_into());
-  assert_eq!(U12::from(255u8).wrapping_mul(2u8.into()), (510 as u16).unchecked_into());
-  assert_eq!(U12::from(2u8).wrapping_mul((2048u16).unchecked_into()), U12::min_value());
-  assert_eq!(U12::from(2u8).wrapping_mul((4095u16).unchecked_into()), (0xFFE as u16).unchecked_into());
+  assert_eq!(u12![2].wrapping_mul(u12![0]), U12::min_value());
+  assert_eq!(u12![2].wrapping_mul(u12![255]), u12![510]);
+  assert_eq!(u12![255].wrapping_mul(u12![2]), u12![510]);
+  assert_eq!(u12![2].wrapping_mul(u12![2048]), U12::min_value());
+  assert_eq!(u12![2].wrapping_mul(u12![4095]), u12![0xFFE]);
 }
 
 // MARK: - Tests - Division
 
 #[test]
 fn test_div_operator() {
-  assert_eq!(U12::max_value() / U12::max_value(), U12::from(1u8));
-  assert_eq!(U12::from(2u8) / U12::from(255u8), U12::from(0u8));
-  assert_eq!(U12::from(255u8) / U12::from(2u8), U12::from(127u8));
+  assert_eq!(U12::max_value() / U12::max_value(), u12![1]);
+  assert_eq!(u12![2] / u12![255], u12![0]);
+  assert_eq!(u12![255] / u12![2], u12![127]);
 }
 
 #[test]
 #[should_panic]
 fn test_div_operator_divide_by_zero() {
-  let _ = U12::from(2u8) / U12::from(0u8);
+  let _ = u12![2] / u12![0];
 }
 
 #[test]
 fn test_checked_div() {
-  assert_eq!(U12::max_value().checked_div(U12::max_value()), Some(U12::from(1u8)));
-  assert_eq!(U12::from(2u8).checked_div(U12::from(255u8)), Some(U12::from(0u8)));
-  assert_eq!(U12::from(255u8).checked_div(U12::from(2u8)), Some(U12::from(127u8)));
-  assert_eq!(U12::from(255u8).checked_div(U12::from(1u8)), Some(U12::from(255u8)));
-  assert_eq!(U12::from(255u8).checked_div(U12::from(0u8)), None);
+  assert_eq!(U12::max_value().checked_div(U12::max_value()), Some(u12![1]));
+  assert_eq!(u12![2].checked_div(u12![255]), Some(u12![0]));
+  assert_eq!(u12![255].checked_div(u12![2]), Some(u12![127]));
+  assert_eq!(u12![255].checked_div(u12![1]), Some(u12![255]));
+  assert_eq!(u12![255].checked_div(u12![0]), None);
 }
 
 #[test]
 fn test_wrapping_div() {
-  assert_eq!(U12::max_value().wrapping_div(U12::max_value()), U12::from(1u8));
-  assert_eq!(U12::from(2u8).wrapping_div(U12::from(255u8)), U12::from(0u8));
-  assert_eq!(U12::from(255u8).wrapping_div(U12::from(2u8)), U12::from(127u8));
-  assert_eq!(U12::from(255u8).wrapping_div(U12::from(1u8)), U12::from(255u8));
+  assert_eq!(U12::max_value().wrapping_div(U12::max_value()), u12![1]);
+  assert_eq!(u12![2].wrapping_div(u12![255]), u12![0]);
+  assert_eq!(u12![255].wrapping_div(u12![2]), u12![127]);
+  assert_eq!(u12![255].wrapping_div(u12![1]), u12![255]);
 }
 
 #[test]
 #[should_panic]
 fn test_wrapping_div_overflow() {
-  let _ = U12::from(255u8).wrapping_div(U12::from(0u8));
+  let _ = u12![255].wrapping_div(u12![0]);
 }
 
 // MARK: - Tests - Negation
 
 #[test]
 fn test_checked_neg() {
-  assert_eq!(U12::from(0u8).checked_neg(), Some(0u8.into()));
-  assert_eq!(U12::from(1u8).checked_neg(), None);
-  assert_eq!(U12::from(2u8).checked_neg(), None);
-  assert_eq!(U12::from(255u8).checked_neg(), None);
+  assert_eq!(u12![0].checked_neg(), Some(u12![0]));
+  assert_eq!(u12![1].checked_neg(), None);
+  assert_eq!(u12![2].checked_neg(), None);
+  assert_eq!(u12![255].checked_neg(), None);
 }
 
 #[test]
 fn test_wrapping_neg() {
-  assert_eq!(U12::from(0u8).wrapping_neg(), 0u8.into());
-  assert_eq!(U12::from(1u8).wrapping_neg(), 0xFFFu16.unchecked_into());
-  assert_eq!(U12::from(2u8).wrapping_neg(), 0xFFEu16.unchecked_into());
-  assert_eq!(U12::from(255u8).wrapping_neg(), 0xF01u16.unchecked_into());
+  assert_eq!(u12![0].wrapping_neg(), u12![0]);
+  assert_eq!(u12![1].wrapping_neg(), u12![0xFFF]);
+  assert_eq!(u12![2].wrapping_neg(), u12![0xFFE]);
+  assert_eq!(u12![255].wrapping_neg(), u12![0xF01]);
 }
 
 #[test]
 fn test_overflowing_neg() {
-  assert_eq!(U12::from(0u8).overflowing_neg(), (0u8.into(), false));
-  assert_eq!(U12::from(1u8).overflowing_neg(), (0xFFFu16.unchecked_into(), true));
-  assert_eq!(U12::from(2u8).overflowing_neg(), (0xFFEu16.unchecked_into(), true));
-  assert_eq!(U12::from(255u8).overflowing_neg(), (0xF01u16.unchecked_into(), true));
+  assert_eq!(u12![0].overflowing_neg(), (u12![0], false));
+  assert_eq!(u12![1].overflowing_neg(), (u12![0xFFF], true));
+  assert_eq!(u12![2].overflowing_neg(), (u12![0xFFE], true));
+  assert_eq!(u12![255].overflowing_neg(), (u12![0xF01], true));
 }
